34c113c893c80bda8705b55b3a276e1b709d7555
Israel0x <125441958+gb0g0@users.noreply.github.com>
2023-11-16 11:47:10 +0000

    home

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { logo } from '../assets'
 import { navlinks } from '../constants'
 
@@ -23,9 +23,9 @@ const Sidebar = () => {
   const [isActive, setIsActive] = useState('dashboard')
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      <Link to="/">
+      <a href="https://crowdseed-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-      </Link>
+      </a>
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
